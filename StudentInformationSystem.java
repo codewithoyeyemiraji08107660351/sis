498721281deb1e5da139d1db8f7161c69622a330
Oyeyemi Raji <babatundeoyeyemi7@gmail.com>
2024-01-13 20:55:13 +0100

    student information syste design

---
+++ StudentInformationSystem.java
@@ -0,0 +1,17 @@
+
+package sis;
+
+/**
+ *
+ * @author AmTechGuy
+ */
+public class StudentInformationSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
